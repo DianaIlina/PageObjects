16db7b19cf357c21fd24cdf49f0f005aa3b66b43
Диана Ильина <diana.1993.30@gmail.com>
2022-07-15 15:59:54 +0100

    Fixing time duration

--- src/test/java/ru/netology/web/page/CardPage.java
+++ src/test/java/ru/netology/web/page/CardPage.java
@@ -1,9 +1,18 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.Condition;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CardPage {
-
+    public CardPage() {
+        $x("//*/h2[@data-test-id=\"dashboard\"]")
+                .shouldHave(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Личный кабинет"));    }
     public void Transfer(String receiveCardId, int transferringAmount, String withdrawCardNumberFull) {
         $x("//*/div[@data-test-id=\"" + receiveCardId + "\"]//button").click();
         $x("//*/span[@data-test-id=\"amount\"]//input").setValue(String.valueOf(transferringAmount));
